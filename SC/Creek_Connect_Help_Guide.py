0a1a10ab7d00af58f60b71e6053a6291773cf485
Helen Wan <155197028+WanHelen@users.noreply.github.com>
2024-04-01 14:37:33 -0600

    Create Creek_Connect_Help_Guide.py

---
+++ SC/Creek_Connect_Help_Guide.py
@@ -0,0 +1,6 @@
+Creek Connect Help Guide
+Search: Type in the space bar to search for a partner.
+Save Information: Save the information of a business partner to your files.
+Add New Partner: Use this button to input new business and community partners into Creek Connect. It's a simple form that lets you expand your network
+Clear: Reset the tab using this button.
+Logout: Safely exit your session with this button. It ensures that your information remains secure when you're done using the application.
